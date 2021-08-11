e45c914d9f8a7c664525d7fe749671b6f30b8b0b
hirohamada12 <nhatanhvt123@gmail.com>
2021-08-11 15:31:41 +0700

    fixed link

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "www.facebook.com/anhvtnfpt";
+            window.location = "https://fb.com/anhvtnfpt";
           },
         });
       }
